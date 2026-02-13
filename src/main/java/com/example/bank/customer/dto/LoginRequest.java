08a7d735b561b57dd37727cddeda1b976c976560
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-13 14:51:47 +0100

    Create LoginRequest.java

---
+++ src/main/java/com/example/bank/customer/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.bank.customer.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password
+) {
+}
