7ab2aa135468a83310a9627d732a06c8e5aa22b4
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-13 14:51:03 +0100

    Create CreateAccountRequest.java

---
+++ src/main/java/com/example/bank/account/dto/CreateAccountRequest.java
@@ -0,0 +1,10 @@
+package com.example.bank.account.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateAccountRequest(
+        @NotBlank @Size(min = 3, max = 3)
+        String currency
+) {
+}
