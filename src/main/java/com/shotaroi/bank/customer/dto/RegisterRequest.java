bc11fe5d80a876574813135c702f09d7368f5202
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-21 04:55:49 +0100

    Create RegisterRequest.java

---
+++ src/main/java/com/shotaroi/bank/customer/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.shotaroi.bank.customer.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @NotBlank @Email
+        String email,
+
+        @NotBlank @Size(min = 8)
+        String password
+) {
+}
