1feeb62dd67a73620eae97209aee5340962a1c7f
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-22 00:58:48 +0100

    Create JwtProperties.java

---
+++ src/main/java/com/shotaroi/bank/config/JwtProperties.java
@@ -0,0 +1,7 @@
+package com.shotaroi.bank.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(String secret, long expirationMs) {
+}
