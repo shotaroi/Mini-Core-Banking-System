e27934d478df33b72e40e8f8a6d7d6013814e497
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-22 00:58:36 +0100

    Create CustomerRepository.java

---
+++ src/main/java/com/shotaroi/bank/customer/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.shotaroi.bank.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
